8988effbdadd4d04c4fdbb9d5c6dd62b89a975a2
Vikas Pogu <vikaspoguadf@gmail.com>
2024-07-23 09:30:15 -0400

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+# LogIt
+
+### Open-source, opinionated mobile app for logging your procedures 
+
+[screenshots made by previewed](https://previewed.app/template/00CBF3F6)
+
+[<img src="https://github.com/Vikaspogu/logit-app/assets/58703865/94cbf557-b1a9-4339-b6b4-def21dde3c11"
+    alt="Get it on GitHub"
+    height="80">](https://github.com/Vikaspogu/logit-app/releases/latest)
--- app/src/main/java/com/vikaspogu/logit/ui/util/Constants.kt
+++ app/src/main/java/com/vikaspogu/logit/ui/util/Constants.kt
@@ -1,8 +1,8 @@
 package com.vikaspogu.logit.ui.util
 
 object Constants {
-    const val GITHUB_RELEASES_LINK = "https://github.com/vikaspogu"
-    const val PROJECT_GITHUB_LINK = "https://github.com/vikaspogu"
+    const val GITHUB_RELEASES_LINK = "https://github.com/Vikaspogu/logit-app/releases"
+    const val PROJECT_GITHUB_LINK = "https://github.com/Vikaspogu/logit-app.git"
     const val ADD = "add"
     const val EDIT = "edit"
 }
